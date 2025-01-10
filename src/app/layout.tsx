21703caa3bae7462b0a38509e9a66a5108855d1f
DuyNam <vo.duynam@gmail.com>
2025-01-10 18:19:29 +1000

    feat: recreate header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,41 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import RalleyLogo from "@/assets/RalleyIcon.svg";
+import { Button } from "@/components/ui/button.tsx";
+
+const menuChoices: { text: string; url: string }[] = [
+  { text: "What's on", url: "/whatson" },
+  { text: "Communities", url: "/communities" },
+  { text: "Resources", url: "/resources" },
+  { text: "Overview", url: "/overview" },
+];
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-background-subtle">
       <header className="fixed top-0 left-0 right-0 z-50 border-b bg-white">
         <div className="pl-14 h-20 flex">
-          <span className="m-auto">Ralley</span>
+          <img
+            src={RalleyLogo}
+            alt={"ralley-logo"}
+            className={"w-[128px] h-[36px] my-auto mr-auto"}
+          />
+          <div className="m-auto">
+            {menuChoices.map((choice) => {
+              return (
+                <Button variant={"ghost"} key={choice.text}>
+                  {choice.text}
+                </Button>
+              );
+            })}
+          </div>
+          <div className="ml-auto my-auto mr-3">
+            <Button variant={"ghost"}>Login</Button>
+            <Button className={"text-primary"} variant={"ghost"}>
+              Create
+            </Button>
+            <Button variant={"default"}>Sign up</Button>
+          </div>
         </div>
       </header>
       <SidebarProvider>
