9c7045ee8b2ed7d46b6974d104831022ff590500
DuyNam <vo.duynam@gmail.com>
2025-01-11 18:55:03 +1000

    fix: set default route

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router";
-import { Home } from "@/pages/home/home.tsx";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+// import { Home } from "@/pages/home/home.tsx";
 import { Inbox } from "@/pages/inbox/inbox.tsx";
 import { Events } from "@/pages/events/events.tsx";
 
@@ -13,9 +13,10 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
-            <Route index element={<Home />} />
+            <Route index element={<Events />} />
             <Route path="inbox" element={<Inbox />} />
             <Route path="events" element={<Events />} />
+            <Route path="*" element={<Navigate to={"/"} replace />} />
           </Route>
         </Routes>
       </BrowserRouter>
