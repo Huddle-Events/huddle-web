c6f0562b1ade74ce3cd7b527d906f1778fec5470
DuyNam <vo.duynam@gmail.com>
2025-01-12 10:57:45 +1000

    feat: add date formatting for date selection

--- src/components/events/event-dropdown-date.tsx
+++ src/components/events/event-dropdown-date.tsx
@@ -11,7 +11,7 @@ import { ChevronDown } from "lucide-react";
 import { useState } from "react";
 import { Calendar } from "@/components/ui/calendar.tsx";
 import { DateRange } from "react-day-picker";
-import { add } from "date-fns";
+import { add, format } from "date-fns";
 
 const dateList = [
   "Today",
@@ -32,12 +32,26 @@ const EventDropdownDate = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="dropdown">
-          <span className={"font-normal text-sm text-color-text"}>
-            All dates
-          </span>
-          <ChevronDown />
-        </Button>
+        {selectedDateType === "Custom" ? (
+          <Button variant="dropdown">
+            <span className={"font-normal text-sm text-color-text"}>From:</span>
+            <span className={"font-semibold text-sm text-color-text"}>
+              {format(date?.from ?? new Date(), "d MMM yyyy")}
+            </span>
+            -<span className={"font-normal text-sm text-color-text"}>To:</span>
+            <span className={"font-semibold text-sm text-color-text"}>
+              {format(date?.to ?? new Date(), "d MMM yyyy")}
+            </span>
+            <ChevronDown />
+          </Button>
+        ) : (
+          <Button variant="dropdown">
+            <span className={"font-normal text-sm text-color-text"}>
+              All dates
+            </span>
+            <ChevronDown />
+          </Button>
+        )}
       </DropdownMenuTrigger>
       {selectedDateType !== "Custom" ? (
         <DropdownMenuContent className="w-56">
@@ -69,6 +83,7 @@ const EventDropdownDate = () => {
             selected={date}
             onSelect={setDate}
             className="rounded-md border"
+            numberOfMonths={2}
           />
           <DropdownMenuItem>
             <Button className={"w-full bg-black"}>Apply</Button>
